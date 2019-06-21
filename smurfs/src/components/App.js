f1118d46f4db1149b315273bc70209ad53ffce26
Andrew Dillon <majik895@gmail.com>
2019-06-21 14:22:13 -0400

    Added new error

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,99 +1,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getSmurfs, addSmurf, deleteSmurf } from '../actions';
-import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
-class App extends Component {
-  state = {
-    name: '',
-    age: '',
-    height: ''
-  };
+import { getSmurfs, addSmurf, deleteSmurf, updateSmurf } from '../actions';
 
+import Form from './Form';
+import SmurfList from './SmurfsList';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      age: '',
+      height: ''
+    };
+  }
   componentDidMount() {
     this.props.getSmurfs();
   }
 
-  handleInput = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  addSmurf = e => {
-    e.preventDefault();
-
-    const { name, age, height } = this.state;
+  handleAddSmurf = ({ name, age, height }) => {
+    // e.preventDefault();
     this.props.addSmurf({ name, age, height });
-    this.setState({ name: '', age: '', height: '' });
-  }
+  };
 
-  deleteSmurf = (e, id) => {
-    e.preventDefault();
+  handleUpdateSmurf = ({ id, name, age, height }) => {
+    // e.preventDefault();
+    this.props.updateSmurf({ id, name, age, height });
+  };
+
+  handleDeleteSmurf = id => {
     this.props.deleteSmurf(id);
-  }
+  };
 
   render() {
-    const { smurfs, fetchingSmurfs, error } = this.props;
-    const { name, age, height } = this.state;
-
-    if (fetchingSmurfs) {
-      return <h2 className="App">Loading Smurfs...</h2>;
-    } else if (error) {
-      return <h2 className="App">404 Smurfs Not Found</h2>;
-    } else {
-      return (
-        <div className="App">
-          <h1>SMURFS! 2.0 W/ Redux</h1>
-
-          <h3>Add new Smurf</h3>
-
-          <form onSubmit={this.addSmurf}>
-            <input
-              placeholder="Enter Name"
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleInput}
-            />
-
-            <input
-              placeholder="Enter Age"
-              type="text"
-              name="age"
-              value={age}
-              onChange={this.handleInput}
-            />
-
-            <input
-              placeholder="Enter Height"
-              type="text"
-              name="height"
-              value={height}
-              onChange={this.handleInput}
-            />
-
-            <button type="submit">Add Smurf</button>
-          </form>
-
-          <smurfs smurfs={smurfs} deleteSmurf={this.deleteSmurf} />
-        </div>
-      );
-    }
+    return (
+      <div className="App">
+        {this.props.updatingSmurf ? (
+          <Form handleUpdateSmurf title="Update Smurf" />
+        ) : (
+          <Form handleAddSmurf={this.handleAddSmurf} title="Add Smurf" />
+        )}
+        {this.props.fetchingSmurfs ? (
+          <div>Loading...</div>
+        ) : (
+          <SmurfList
+            smurfs={this.props.smurfs}
+            handleDeleteSmurf={this.handleDeleteSmurf}
+            handleUpdateSmurf={this.handleUpdateSmurf}
+          />
+        )}
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
-  console.log('State from App', state)
   return {
     smurfs: state.smurfs,
     fetchingSmurfs: state.fetchingSmurfs,
-    addingSmurf: state.addingSmurf,
-    error: state.error
+    updatingSmurf: state.updatingSmurf
   };
 };
 
-export default connect(mapStateToProps, { getSmurfs, addSmurf, deleteSmurf })(App);
+export default connect(
+  mapStateToProps,
+  { getSmurfs, addSmurf, deleteSmurf, updateSmurf }
+)(App);
--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Smurf = props => {
-  const { smurf } = props;
-  return (
-    <div>
-      <p>ID: {smurf.id}</p>
-      <p>Name: {smurf.name}</p>
-      <p>Age: {smurf.age}</p>
-      <p>Height: {smurf.height}</p>
-      <button type="submit" onClick={(e) => props.deleteSmurf(e, smurf.id)}>Delete</button>
-      <hr />
-    </div>
-  );
+class Smurf extends Component {
+  state = {
+    name: this.props.smurf.name,
+    age: this.props.smurf.age,
+    height: this.props.smurf.height,
+    id: this.props.smurf.id,
+    edit: false
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleEditSmurf = () => {
+    this.setState({ edit: true });
+  };
+
+  updateSmurf = e => {
+    e.preventDefault();
+    const { name, age, height, id } = this.state;
+    this.props.handleUpdateSmurf({ id, name, age, height });
+    this.setState({ edit: false });
+  };
+
+  render() {
+    const { name, age, height, id, edit } = this.state;
+    const { handleDeleteSmurf } = this.props;
+    return (
+      <div className="card">
+        {edit ? (
+          <React.Fragment>
+            <input value={name} name="name" onChange={this.handleInputChange} />
+            <input
+              value={height}
+              name="height"
+              onChange={this.handleInputChange}
+            />
+            <input value={age} name="age" onChange={this.handleInputChange} />
+            <div className="btns">
+              <button className="btn btn-edit" onClick={this.updateSmurf}>
+                Update
+              </button>
+            </div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <h3>{name}</h3>
+            <p>{height}</p>
+            <p>{age}</p>
+            <div className="btns">
+              <button
+                className="btn btn-remove"
+                onClick={() => handleDeleteSmurf(id)}
+              >
+                Remove
+              </button>
+              <button className="btn btn-edit" onClick={this.handleEditSmurf}>
+                Edit
+              </button>
+            </div>
+          </React.Fragment>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Smurf;
